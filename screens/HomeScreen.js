9818f42fc9be47c4710fd3165826ce1edfe380c5
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-07-18 19:03:34 +0300

    Added icons and styles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,8 +7,81 @@ const HomeScreen = () => {
             <Text>
                 Where educaction is empowered anytime, anywhere.
             </Text>
+            <View style={styles.signup}>
+                <Text style={styles.signupText}>Sign Up as:</Text>
+                <TouchableOpacity style={styles.signupOption} onPress={handleStudentPress}>
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={40}
+                        style={styles.icon}
+                    />
+                    <Text style={styles.optionText}>Student</Text>
+                </TouchableOpacity>
+
+                
+                <TouchableOpacity style={styles.signupOption} onPress={handleInstructorPress}>
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={40}
+                        style={styles.icon}
+                    />
+                    <Text style={styles.optionText}>Instructor</Text>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity style={styles.signupOption} onPress={handleSchoolPress}>
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={40}
+                        style={styles.icon}
+                    />
+                    <Text style={styles.optionText}>School</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+      justifyContent: 'center',  
+      alignItems: 'center',      
+    },
+    text: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 24,
+      marginBottom: 50,  
+    },
+    signup: {
+      backgroundColor: '#584D4D',
+      padding: 20,       
+      borderRadius: 5,
+      width: '80%',     
+      alignItems: 'center',
+      height: 500 
+    },
+    signupText: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 30,  
+    },
+    signupOption: {
+      alignItems: 'center',   
+      marginBottom: 60,       
+    },
+    icon: {
+      marginBottom: 10,       
+    },
+    optionText: {
+      fontSize: 18,
+      color: 'white',
+    },
+  });
 
 export default HomeScreen;
