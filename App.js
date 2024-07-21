e0691453eddc23de07e3416dfb02140f756e5811
jolene <sitienijolene@gmail.com>
2024-07-21 13:01:11 +0300

    create the teache landing page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import StudentLanding from './landingpages/StudentLanding';
+import TeacherLanding from './landingpages/TeacherLanding';
 
 export default function App() {
   return (
-    <StudentLanding />
+    // <StudentLanding />
+    <TeacherLanding />
   );
 }
 
---
+++ landingpages/TeacherLanding.js
@@ -0,0 +1,173 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Dimensions, SafeAreaView } from "react-native";
+import { Card, Text } from "react-native-elements";
+
+// Get screen dimensions
+const { width, height } = Dimensions.get('window');
+// Calculate card dimensions based on screen size
+const cardWidth = width * 0.44;
+const cardHeight = height * 0.25;
+
+const TeacherLanding = () => {
+    return (
+        // SafeAreaView ensures content is displayed within the safe area boundaries of the device
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                {/* Header section */}
+                <View style={styles.headerContainer}>
+                    <Text style={styles.welcomeText}>Welcome,</Text>
+                    <Text style={styles.nameText}>Mary Olwal</Text>
+                </View>
+            
+
+                {/* cards container */}
+                <View style={styles.cardsContainer}>
+                    {/* profile card */}
+                    <TouchableOpacity style={styles.cardWrapper}>
+                        {/* card component with dynamic width and height */}
+                        <Card containerStyle={[styles.card, { width: cardWidth, height: cardHeight }]}>
+                            {/* card content */}
+                            <View style={styles.cardContent}>
+                                {/* card image */}
+                                <Card.Image
+                                    source={require('../assets/images/account-avatar-profile-user-6-svgrepo-com.svg')}
+                                    style={styles.cardImage}
+                                />
+                                {/* card title */}
+                                <Text style={styles.cardTitle}>Profile</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+
+                    {/* course work card */}
+                    <TouchableOpacity style={styles.cardWrapper}>
+                        {/* card component with dynamic width and height */}
+                        <Card containerStyle={[styles.card, { width: cardWidth, height: cardHeight }]}>
+                            {/* card content */}
+                            <View style={styles.cardContent}>
+                                {/* card image */}
+                                <Card.Image
+                                    source={require('../assets/images/account-avatar-profile-user-6-svgrepo-com.svg')}
+                                    style={styles.cardImage}
+                                />
+                                {/* card title */}
+                                <Text style={styles.cardTitle}>Course work</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+
+                    {/* library card */}
+                    <TouchableOpacity style={styles.cardWrapper}>
+                        {/* card component with dynamic width and height */}
+                        <Card containerStyle={[styles.card, { width: cardWidth, height: cardHeight }]}>
+                            {/* card content */}
+                            <View style={styles.cardContent}>
+                                {/* card image */}
+                                <Card.Image
+                                    source={require('../assets/images/account-avatar-profile-user-6-svgrepo-com.svg')}
+                                    style={styles.cardImage}
+                                />
+                                {/* card title */}
+                                <Text style={styles.cardTitle}>Library</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+
+                    {/* attendance card */}
+                    <TouchableOpacity style={styles.cardWrapper}>
+                        {/* card component with dynamic width and height */}
+                        <Card containerStyle={[styles.card, { width: cardWidth, height: cardHeight }]}>
+                            {/* card content */}
+                            <View style={styles.cardContent}>
+                                {/* card image */}
+                                <Card.Image
+                                    source={require('../assets/images/account-avatar-profile-user-6-svgrepo-com.svg')}
+                                    style={styles.cardImage}
+                                />
+                                {/* card title */}
+                                <Text style={styles.cardTitle}>Attendance</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+
+                    {/* class room card */}
+                    <TouchableOpacity style={styles.cardWrapper}>
+                        {/* card component with dynamic width and height */}
+                        <Card containerStyle={[styles.card, { width: cardWidth, height: cardHeight }]}>
+                            {/* card content */}
+                            <View style={styles.cardContent}>
+                                {/* card image */}
+                                <Card.Image
+                                    source={require('../assets/images/account-avatar-profile-user-6-svgrepo-com.svg')}
+                                    style={styles.cardImage}
+                                />
+                                {/* card title */}
+                                <Text style={styles.cardTitle}>Classroom</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#000000', 
+    },
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    headerContainer: {
+        marginBottom: 20,
+    },
+    welcomeText: {
+        fontSize: 25, 
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    nameText: {
+        fontSize: 35, 
+        fontWeight: 'bold',
+        color: 'white', 
+    },
+    cardsContainer: {
+        flexDirection: 'row', 
+        flexWrap: 'wrap', // Allow wrapping to next line
+        justifyContent: 'space-between', 
+    },
+    cardWrapper: {
+        marginBottom: 15,
+    },
+    card: {
+        padding: 10,
+        margin: 0,
+        backgroundColor: '#584D4D',
+        borderRadius: 15, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0,
+        shadowColor: 'transparent',        
+    },
+    cardContent: {
+        alignItems: 'center', 
+    },
+    cardImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25, 
+        marginBottom: 10,
+    },
+    cardTitle: {
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        fOntSize: 16, // Larger font size for readability
+    },
+});
+
+export default TeacherLanding;
