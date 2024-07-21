99813dbb29237c25af4d79ffba090038869bf8b7
jolene <sitienijolene@gmail.com>
2024-07-21 13:07:57 +0300

    create the owner landing page

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import StudentLanding from './landingpages/StudentLanding';
 import TeacherLanding from './landingpages/TeacherLanding';
+import OwnerLanding from './landingpages/OwnerLanding';
 
 export default function App() {
   return (
     // <StudentLanding />
-    <TeacherLanding />
+    //<TeacherLanding />
+    <OwnerLanding />
   );
 }
 
